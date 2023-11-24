8757317a34172628fbdc16565b8e1c4bf938da1c
doobie-droid <lesliedouglas23@gmail.com>
2023-11-24 19:03:34 +0100

    profile settings menu has been finished and done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const alertDialog = document.querySelector("#alert-dialog");
 
 const modalCloserFunction = function () {
   alertDialog.remove();
-  document.querySelector("section.setup").classList.add("move-up");
+  document.querySelector("section.setup").classList.add("move-up-setup");
 
 };
 
